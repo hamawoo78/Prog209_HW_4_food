22dda91dfd9adc30e08452d8c609b72a4569b750
hamawoo78 <nae.yk.jp@gmail.com>
2023-04-26 18:52:52 -0700

    Here is some ourfirst repo!

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById("track").addEventListener("click", function(){
+        // submit button function
+
+    });
+});
