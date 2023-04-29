394c15714186ec698147bf7573f83beac8852bfb
hamawoo78 <nae.yk.jp@gmail.com>
2023-04-29 16:05:46 -0700

    4/29 I added some function - Nae

--- library_food.js
+++ library_food.js
@@ -3,3 +3,20 @@ let Food = function (pName, pCalories) {
     this.food = pName;
     this.calories = parseInt(pCalories); 
 };
+
+Food.prototype.isValid = function () {
+    if (this.food == "" || isNaN(this.calories) || this.calories <= 0) 
+    {
+        return false;
+    } 
+    else 
+    {
+        return true;
+    }
+};
+
+Food.prototype.toString = function () {
+    let cal = this.calculateMpg().toFixed(1);
+    return this.calories;
+};
+
--- main.js
+++ main.js
@@ -1,16 +1,37 @@
+let foods = [];
+
 document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("track").addEventListener("click", newFood);
 });
 
 // submit button function
 let newFood = function () {    
-
-    displayTotalCalories();
+    let food = new Food(
+        document.getElementById("food").value,
+        document.getElementById("calories").value
+    );
+    
+    if (!food.isValid()) {
+        alert("Please complete all fields. " +
+            "Calories must be numeric " +
+            "and greater than zero.");
+    } else {
+        foods.push(food);  // add new food information to array
+        console.log(foods);
+        console.log(foods.length);
+        displayTotalCalories();
+    }
+    document.getElementById("food").focus();
 }
 
 let displayTotalCalories = function () {
-    let displayString = "";
+    let displayString = "", calTotal = 0;
 
+    for (let i in foods) {  
+        calTotal = calTotal + foods[i].calories;                          
+    }
+
+    displayString = calTotal.toString() ; 
 
     document.getElementById("total").value = displayString; // update new Calories
     document.getElementById("food").select();           // make it easy to enter a new food
